7e1de8a06db5eb3b23ba59199c136e8f4e82a03a
camilo <camiloporto@gmail.com>
2015-07-06 15:20:16 -0300

    refatorado testes. adicionado @Before para criacao do objeto a ser testado

--- src/main/java/br/com/caelum/agiletickets/maiormenor/MaiorMenorTest.java
+++ src/main/java/br/com/caelum/agiletickets/maiormenor/MaiorMenorTest.java
@@ -1,13 +1,21 @@
 package br.com.caelum.agiletickets.maiormenor;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MaiorMenorTest {
 	
+	private MaiorMenor maiorMenor;
+
+	@Before
+	public void setup() {
+		maiorMenor = new MaiorMenor();
+		System.out.println("Oi!");
+	}
+	
 	@Test
 	public void numero_em_qualquer_ordem() {
-		MaiorMenor maiorMenor = new MaiorMenor();
 		maiorMenor.encontra(new int[]{4, 15, 7, 8});
 		
 		Assert.assertEquals(15, maiorMenor.getMaior());
@@ -17,7 +25,6 @@ public class MaiorMenorTest {
 	
 	@Test
 	public void numero_em_ordem_decrescente() {
-		MaiorMenor maiorMenor = new MaiorMenor();
 		maiorMenor.encontra(new int[]{5, 4, 3});
 		
 		Assert.assertEquals(5, maiorMenor.getMaior());
@@ -27,7 +34,6 @@ public class MaiorMenorTest {
 	
 	@Test
 	public void numero_em_ordem_crescente() {
-		MaiorMenor maiorMenor = new MaiorMenor();
 		maiorMenor.encontra(new int[]{3, 4, 5});
 		
 		Assert.assertEquals(5, maiorMenor.getMaior());
@@ -37,7 +43,6 @@ public class MaiorMenorTest {
 	
 	@Test
 	public void lista_com_apenas_um_elemento() {
-		MaiorMenor maiorMenor = new MaiorMenor();
 		maiorMenor.encontra(new int[]{4});
 		
 		Assert.assertEquals(4, maiorMenor.getMaior());
